d0e0bc47669a804bca5f22170a4e343c21c13133
dandimuzaki <dandimuzaki@gmail.com>
2026-07-02 22:20:09 +0700

    bugs: fix axios api

--- src/services/predict.service.ts
+++ src/services/predict.service.ts
@@ -3,6 +3,7 @@ import { api } from "../lib/api"
 export const predictService = {
   async predict(form: FormData) {
     const response = await api.post("/plant_disease_detection/predict?model=efficientnet", form);
+    console.log(response)
     return response;
   },
 };
