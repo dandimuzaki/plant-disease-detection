f255dc8592bd110c62a26937175f0836cc1aa938
dandimuzaki <dandimuzaki@gmail.com>
2026-07-02 09:46:39 +0700

    feat: modify routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,4 +1,4 @@
-import type { RouteObject } from "react-router";
+import { Navigate, type RouteObject } from "react-router";
 import HomePage from "./pages/HomePage";
 import ResultPage from "./pages/ResultPage";
 import MainLayout from "./layouts/MainLayout";
@@ -7,6 +7,10 @@ const routes: RouteObject[] = [
   {
     element: <MainLayout />,
     children: [
+      {
+        index: true,
+        element: <Navigate to="/home" replace />,
+      },
       {
         path: "/home",
         element: <HomePage />,
